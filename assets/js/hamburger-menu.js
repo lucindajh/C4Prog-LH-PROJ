be942c9b8a59ac9e8dc36dbb558a882b743e396e
20110825 <20110825@tafe.wa.edu.au>
2024-12-01 13:31:22 +0800

    fix: mobile nav now disappears when window is resized to large

--- assets/js/hamburger-menu.js
+++ assets/js/hamburger-menu.js
@@ -1,13 +1,22 @@
 const hamburgerIcon = document.getElementById('hamburger-icon');
 const mobileNav = document.getElementById('mobile-nav');
 
-hamburgerIcon.addEventListener('click', () => {
-    if (mobileNav.style.display === 'block'){
+
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 1024){
+        hamburgerIcon.addEventListener('click', () => {
+            if (mobileNav.style.display === 'block'){
+                mobileNav.style.display = 'none'
+            }
+        
+            else {
+                mobileNav.style.display = 'block'
+            }
+            
+        })
+    }
+    else {
         mobileNav.style.display = 'none'
     }
-
-    else {
-        mobileNav.style.display = 'block'
-    }
-    
 })
